d396d16f79ac054c947f6c379033800d9c29b43d
andreivercosa <andreivercosa@gmail.com>
2019-02-28 21:42:44 -0400

    v1

---
+++ CalcApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalcApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void calcular(object sender, EventArgs e)
+        {
+
+            int resutado = Convert.ToInt32(Num1.Text) + Convert.ToInt32(Num2.Text);
+            texto.Text = "Resultado da soma: " + resutado;
+        }
+    }
+}
